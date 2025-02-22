d373ba03073f8ca9fc172b8a89ac2a6b90e4f3df
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2025-02-22 16:21:09 +0200

    SOLVED: Donations

--- Donations/src/Donations.sol
+++ Donations/src/Donations.sol
@@ -10,5 +10,7 @@ contract Donations {
         // the person donated
         // don't forget a person can donate
         // multiple times!
+
+        amountDonated[msg.sender] += msg.value;
     }
 }
