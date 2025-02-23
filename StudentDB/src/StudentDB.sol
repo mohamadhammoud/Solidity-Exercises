76772a5fe22b4ee7f92fd98c6b9efaa24d20727e
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2025-02-23 23:47:36 +0200

    SOLVED: StudentDB

--- StudentDB/src/StudentDB.sol
+++ StudentDB/src/StudentDB.sol
@@ -20,10 +20,13 @@ contract StudentDB {
     // create Student's data
     function createStudent(string memory _name, uint256 _age) public {
         // your code here
+        student.name = _name;
+        student.age = _age;
     }
 
     // return struct data
     function getEntireStruct() public view returns (Student memory) {
         // your code here
+        return student;
     }
 }
