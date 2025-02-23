b37cf9c8b333bc7675a58d10655161408e8f500f
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2025-02-23 21:12:54 +0200

    SOLVED: OneWeekLockup

--- OneWeekLockup/src/OneWeekLockup.sol
+++ OneWeekLockup/src/OneWeekLockup.sol
@@ -12,16 +12,24 @@ contract OneWeekLockup {
      * - withdrawEther(uint256 )
      * - balanceOf(address )
      */
-
+    mapping(address => uint256) private _balances;
+    mapping(address => uint256) private _lastDepositTime;
     function balanceOf(address user) public view returns (uint256) {
         // return the user's balance in the contract
+        return _balances[user];
     }
 
     function depositEther() external payable {
         /// add code here
+        _balances[msg.sender] += msg.value;
+        _lastDepositTime[msg.sender] = block.timestamp;
     }
 
     function withdrawEther(uint256 amount) external {
-        /// add code here
+        /// add code herer
+        require((block.timestamp) > (_lastDepositTime[msg.sender] + 1 weeks));
+        require(amount <= _balances[msg.sender]);
+        _balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
     }
 }
